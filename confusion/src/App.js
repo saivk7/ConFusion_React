3a5a36b2fcda9d9007aebf1f0ab789aa97b660eb
Sai Bobby <saivkadiyala97@gmail.com>
2020-11-10 16:19:36 -0500

    added navbar to the app

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar } from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        <Navbar dark color="primary">
+          <div className="container">
+
+            <NavbarBrand href="/"> Ristorante confusion</NavbarBrand>
+
+          </div>
+          
+
+
+        </Navbar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
