cadf75f311eec52c966e49e80ad2e87c57ce2d58
Sai Bobby <saivkadiyala97@gmail.com>
2020-11-11 15:22:13 -0500

    react components part1 w1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
         
         <Navbar dark color="primary">
           <div className="container">
-
-            <NavbarBrand href="/"> Ristorante confusion</NavbarBrand>
-
+            <NavbarBrand href="/"> Ristorante conFusion</NavbarBrand>
           </div>
-          
-
-
         </Navbar>
+        <Menu />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
